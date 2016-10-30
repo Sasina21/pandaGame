584da561ccde23534b916d4f368607618b6e2007
Sasina21 <ngunnnn@gmail.com>
2016-10-31 00:24:05 +0700

    รับการกดปุ่ม

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -30,7 +31,11 @@ public class GameScreen extends ScreenAdapter {
         batch.end();
     }
 	 private void update(float delta) {
-	        x += 10;    
+		 if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+	            x -= 10;
+	        }
+	        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+	            x += 10;
+	        }
 	    }
- 
-}
+	 }
